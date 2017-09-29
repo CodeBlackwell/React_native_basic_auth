cbf76bbb4aa75d0ff8b833459ce303acb9737f41
LeChristopher Blackwell <codeblackwell@gmail.com>
2017-09-29 01:18:05 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common'
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+import firebase from 'firebase';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyDRWhtCHPiOw_vo4zoQDp5jYaEEvQ11EEU',
+            authDomain: 'authentication-d506b.firebaseapp.com',
+            databaseURL: 'https://authentication-d506b.firebaseio.com',
+            projectId: 'authentication-d506b',
+            storageBucket: 'authentication-d506b.appspot.com',
+            messagingSenderId: '81185605946'
+        })
+    }
 
     render () {
         return (
         <View>
             <Header headerText={ "Authentication" }/>
-            <Text>An App</Text>
+            <LoginForm />
         </View>
         )
     }
